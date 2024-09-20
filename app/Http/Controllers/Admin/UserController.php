7672b84c9c04a29b6967f81da775a5470ed6e6ed
Kelly Moura <anakellymourab@gmail.com>
2024-09-20 17:12:09 -0300

    Semana 1: 16/09 -> 20/09

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(15);
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        User::create($request->all());
+        return redirect()->route('users.index')->with('success', 'Usuário criado com sucesso!');
+    }
+
+    public function edit(string $id)
+    {
+        //$user = User::where('id', '=', $id)->first();
+        //$user = User::where('id', $id)->first(); // ->firstOrFail();
+        if (!$user = User::find($id))
+        {
+            return redirect()->route('users.index')->with('message', 'Usuário não encontrado!');
+        }
+
+        return view('admin.users.edit', compact('user'));
+    }
+
+    public function update(StoreUserRequest $request, string $id)
+    {
+        if (!$user = User::find($id))
+        {
+            return back()->with('message', 'Usuário não encontrado!');
+        }
+
+        $user->update($request->only
+        ([
+            'name',
+            'email'
+        ]));
+
+        return redirect()->route('users.index')->with('success', 'Usuário atualizado com sucesso!');
+    }
+}
---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,20 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Cadastro')
+
+@section('content')
+
+    <h1>Novo Usuário</h1>
+
+    {{-- @include('admin.includes.alert') --}}
+    <x-alert/>
+
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf()
+        <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}">
+        <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
+        <input type="password" name="password" placeholder="Senha">
+        <button type="submit">Enviar</button>
+    </form>
+
+@endsection
